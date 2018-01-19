18dc63d4cae5b2b5f2eee239c03a1fa46753122e
Max Koning <m.koning4@student.avans.nl>
2018-01-19 03:22:06 +0100

    url goed gezet

--- model/event.model.js
+++ model/event.model.js
@@ -45,26 +45,4 @@ const EventSchema = new Schema({
 
 const Event = mongoose.model('event', EventSchema);
 
-Event.count({}, function (err, count) {
-    if(count < 5){
-        console.log('add a event');
-        const event = new Event({
-            name: "Zaalvoetbal in Breda",
-            price: 5,
-            sportComplex: "Gymzaal Archimedesstraat",
-            sportHall: "Zaal 1",
-            sport: "Voetbal",
-            availability: true
-        });
-        event.save();
-    }
-
-    else {
-        console.log('zit al een game in de db')
-    }
-});
-
-
-
-
 module.exports = Event;

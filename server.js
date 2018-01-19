0bce275dbcd2f5c38abb2c2987e3548a0f37dbd4
Max Koning <m.koning4@student.avans.nl>
2018-01-19 04:02:56 +0100

    dfsgdfsgdfs

--- server.js
+++ server.js
@@ -35,14 +35,16 @@ app.use(session({
     saveUninitialized: false
 }));
 
+
+
 // CORS headers
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});*/
+});
 
 app.use('/api/v1', logregroutes_v1);
 app.use('/api/v1', authroutes_v1); // Deze als eerst om de andere endpoints te beveiligen met web tokens
